420fcb21ae2031f4a669a4ccdf26c081c82a2344
Mike <Mike@DESKTOP-N4GQE4E>
2017-04-18 09:49:17 -0500

    Added utility methods without body as well as a format property with getter and setters for the format.

--- src/edu/wctc/mds/date/DateUtilities.java
+++ src/edu/wctc/mds/date/DateUtilities.java
@@ -5,12 +5,62 @@
  */
 package edu.wctc.mds.date;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 /**
  *
  * @author Mike
  */
 public class DateUtilities {
+
+//    LocalDateTime currentDate = LocalDateTime.now().withMonth(12).withDayOfMonth(26);
+    private DateTimeFormatter format;
+
+    public final DateTimeFormatter getFormat() {
+        return format;
+    }
+
+    public final void setFormat(DateTimeFormatter format) {
+        this.format = format;
+    }
+    
+    public final String toString(LocalDateTime date) {
+        String stringDate = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        return stringDate;
+    }
+
+    public final Date toDate(String stringDate) {
+
+        return null;
+    }
+
+    public final void calculateDateDifferenceInMinutes() {
+
+    }
+
+    public final void calculateDateDifferenceInHours() {
+
+    }
+
+    public final void calculateDateDifferenceInDays() {
+
+    }
+
+    public final void getDateInFuture() {
+
+    }
     
     
-    
+
+//    public static void main(String[] args) {
+//       
+//        DateUtilities du = new DateUtilities();
+//            LocalDateTime currentDate = LocalDateTime.now().withMonth(12).withDayOfMonth(26);
+//    
+//        
+//        System.out.println(du.toString(currentDate));
+//    }
 }
