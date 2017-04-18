3a27a0ed6e6a850d2ba5811508d6d87dbb0e0113
Mike <Mike@DESKTOP-N4GQE4E>
2017-04-18 17:07:36 -0500

    Completed the difference methods, added get future and past date methods. Began adding JAVA DOC

--- src/edu/wctc/mds/date/DateUtilities.java
+++ src/edu/wctc/mds/date/DateUtilities.java
@@ -5,67 +5,171 @@
  */
 package edu.wctc.mds.date;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
- *
- * @author Mike
+ * Utility class to assist in using and handling date and times using the new
+ * Java API (JDK 8). DateTimeFormatter property added so that user can change
+ * format structure of the toString and toDate methods.
+ * 
+ * @author Mike Sanders, msanders11@my.wctc.edu
+ * @version 1.00
+ * 
  */
 public class DateUtilities {
-
-//    LocalDateTime currentDate = LocalDateTime.now().withMonth(12).withDayOfMonth(26);
-    private DateTimeFormatter format;
     
-
-    public final DateTimeFormatter getFormat() {
-        return format;
+    //DateTimeFormatter property for user to adjsut format structure
+    private DateTimeFormatter dateTimeFormat;
+    
+    /**
+     * Method get the current format set to the DateTimeFormatter property
+     * 
+     * @return the current dateTimeFormat structure when called
+     */
+    public final DateTimeFormatter getDateTimeFormat() {
+        return dateTimeFormat;
     }
-
-    public final void setFormat(DateTimeFormatter format) throws IllegalArgumentException {
-        if(format == null){
+    
+    /**
+     * Method sets the format of the DateTimeFormatter property when called and 
+     * provided and valid dateTimeFormat.
+     * @param dateTimeFormat
+     * @throws IllegalArgumentException - if format is null
+     */
+    public final void setDateTimeFormat(DateTimeFormatter dateTimeFormat) throws IllegalArgumentException {
+        if (dateTimeFormat == null) {
             throw new IllegalArgumentException("No or invlaid format provided");
         }
-        this.format = format;
+        this.dateTimeFormat = dateTimeFormat;
     }
-    
-    public final String toString(LocalDateTime date, DateTimeFormatter format) {
+
+    /**
+     * Parse the LocalDateTime object to a String and formats the string based upon 
+     * the format provided.
+     * @param date a LocalDateTime object
+     * @param format a DateTimeFormatter object
+     * @return LocalDateTime formatted as a string according to the pattern from
+     * the DateTimeFormatter
+     * @throws IllegalArgumentException - if date or format is null
+     */
+    public final String toString(LocalDateTime date, DateTimeFormatter format) throws IllegalArgumentException {
+        if(date == null){
+            throw new IllegalArgumentException("No date provided");
+        }
+        setDateTimeFormat(format);
         String stringDate = date.format(format);
         return stringDate;
     }
 
-    public final Date toDate(String stringDate) {
-//        Date date;
-
-        return null;
+    /**
+     * Converts a string date to a Local Date Time, formatted according to the
+     * DateTimeFormatter
+     * @param stringDate a string date value
+     * @param format a DateTimeFormatter object
+     * @return the string parsed as a LocalDateTime object
+     * @throws IllegalArgumentException - if the string date is null or length
+     * is less than 0 or if the format is null
+     */
+    public final LocalDateTime toLocalDateTime(String stringDate, DateTimeFormatter format) throws IllegalArgumentException {
+        if(stringDate == null || stringDate.length() == 0){
+            throw new IllegalArgumentException("Please provide a date");
+        }
+        setDateTimeFormat(format);
+        LocalDateTime dateTime = LocalDateTime.parse(stringDate, dateTimeFormat);
+        return dateTime;
     }
 
-    public final void calculateDateDifferenceInMinutes(LocalDateTime startDateTime, LocalDateTime endDateTime) {
-
+    /**
+     * 
+     * @param startDateTime
+     * @param endDateTime
+     * @return
+     * @throws IllegalArgumentException 
+     */
+    public final long calculateDateDifferenceInMinutes(LocalDateTime startDateTime, LocalDateTime endDateTime) throws IllegalArgumentException {
+        if (startDateTime == null) {
+            throw new IllegalArgumentException("No start date provided");
+        }
+        if (endDateTime == null) {
+            throw new IllegalArgumentException("No end date provided");
+        }
+        Duration differenceInMinutes = Duration.between(startDateTime, endDateTime);
+        return differenceInMinutes.toMinutes();
     }
 
-    public final void calculateDateDifferenceInHours(LocalDateTime startDateTime, LocalDateTime endDateTime) {
-
+    /**
+     * 
+     * @param startDateTime
+     * @param endDateTime
+     * @return
+     * @throws IllegalArgumentException 
+     */
+    public final long calculateDateDifferenceInHours(LocalDateTime startDateTime, LocalDateTime endDateTime) throws IllegalArgumentException {
+        if (startDateTime == null) {
+            throw new IllegalArgumentException("No start date provided");
+        }
+        if (endDateTime == null) {
+            throw new IllegalArgumentException("No end date provided");
+        }
+        Duration diff = Duration.between(startDateTime, endDateTime);
+        return diff.toHours();
     }
 
-    public final void calculateDateDifferenceInDays(LocalDate startDate, LocalDate endDate) {
-
+    /**
+     * 
+     * @param startDateTime
+     * @param endDateTime
+     * @return
+     * @throws IllegalArgumentException 
+     */
+    public final long calculateDateDifferenceInDays(LocalDateTime startDateTime, LocalDateTime endDateTime) throws IllegalArgumentException {
+        if (startDateTime == null) {
+            throw new IllegalArgumentException("No start date provided");
+        }
+        if (endDateTime == null) {
+            throw new IllegalArgumentException("No end date provided");
+        }
+        Duration differenceInDays = Duration.between(startDateTime, endDateTime);
+        return differenceInDays.toDays();
     }
 
-    public final void getDateInFuture(LocalDate currentDate, int numberOfDays) {
-
+    /**
+     * 
+     * @param date
+     * @param numberOfDays
+     * @return
+     * @throws IllegalArgumentException 
+     */
+    public final LocalDate getDateInFuture(LocalDate date, int numberOfDays) throws IllegalArgumentException {
+        LocalDate currentDate = LocalDate.now();
+        if (date == null || date.isBefore(currentDate)) {
+            throw new IllegalArgumentException("Invalid date provided");
+        }
+        if (numberOfDays <= 0) {
+            throw new IllegalArgumentException("Number of days must be greater than 0");
+        }
+        return date.plusDays(numberOfDays);
     }
-    
-    
 
-//    public static void main(String[] args) {
-//       
-//        DateUtilities du = new DateUtilities();
-//            LocalDateTime currentDate = LocalDateTime.now().withMonth(12).withDayOfMonth(26);
-//    
-//        
-//        System.out.println(du.toString(currentDate));
-//    }
+    /**
+     * 
+     * @param date
+     * @param numberOfDays
+     * @return 
+     */
+    public final LocalDate getPastDate(LocalDate date, int numberOfDays) {
+        LocalDate currentDate = LocalDate.now();
+        if (date == null || date.isAfter(currentDate)) {
+            throw new IllegalArgumentException("Invalid date provided");
+        }
+        if (numberOfDays <= 0) {
+            throw new IllegalArgumentException("Number of days must be greater than 0");
+        }
+        return date.minusDays(numberOfDays);
+    }
+
 }
