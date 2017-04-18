db6ca10767f01828c6fae644b30b129eaaa05211
Mike <Mike@DESKTOP-N4GQE4E>
2017-04-18 10:04:44 -0500

    Added utility methods without body as well as a format property with getter and setters for the format. Added method parameters as well

--- src/edu/wctc/mds/date/DateUtilities.java
+++ src/edu/wctc/mds/date/DateUtilities.java
@@ -5,6 +5,7 @@
  */
 package edu.wctc.mds.date;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -17,39 +18,43 @@ public class DateUtilities {
 
 //    LocalDateTime currentDate = LocalDateTime.now().withMonth(12).withDayOfMonth(26);
     private DateTimeFormatter format;
+    
 
     public final DateTimeFormatter getFormat() {
         return format;
     }
 
-    public final void setFormat(DateTimeFormatter format) {
+    public final void setFormat(DateTimeFormatter format) throws IllegalArgumentException {
+        if(format == null){
+            throw new IllegalArgumentException("No or invlaid format provided");
+        }
         this.format = format;
     }
     
-    public final String toString(LocalDateTime date) {
-        String stringDate = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
-
+    public final String toString(LocalDateTime date, DateTimeFormatter format) {
+        String stringDate = date.format(format);
         return stringDate;
     }
 
     public final Date toDate(String stringDate) {
+//        Date date;
 
         return null;
     }
 
-    public final void calculateDateDifferenceInMinutes() {
+    public final void calculateDateDifferenceInMinutes(LocalDateTime startDateTime, LocalDateTime endDateTime) {
 
     }
 
-    public final void calculateDateDifferenceInHours() {
+    public final void calculateDateDifferenceInHours(LocalDateTime startDateTime, LocalDateTime endDateTime) {
 
     }
 
-    public final void calculateDateDifferenceInDays() {
+    public final void calculateDateDifferenceInDays(LocalDate startDate, LocalDate endDate) {
 
     }
 
-    public final void getDateInFuture() {
+    public final void getDateInFuture(LocalDate currentDate, int numberOfDays) {
 
     }
     
