a9cd19a42f97f539b9190abfb4832e97be88bc51
Mike <Mike@DESKTOP-N4GQE4E>
2017-04-18 17:54:17 -0500

    Finished adding java doc to all methods

--- src/edu/wctc/mds/date/DateUtilities.java
+++ src/edu/wctc/mds/date/DateUtilities.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
  * Utility class to assist in using and handling date and times using the new
  * Java API (JDK 8). DateTimeFormatter property added so that user can change
  * format structure of the toString and toDate methods.
- *
+ * 
  * @author Mike Sanders, msanders11@my.wctc.edu
  * @version 1.00
  *
@@ -26,7 +26,6 @@ public class DateUtilities {
 
     /**
      * Method get the current format set to the DateTimeFormatter property
-     *
      * @return the current dateTimeFormat structure when called
      */
     public final DateTimeFormatter getDateTimeFormat() {
@@ -36,7 +35,6 @@ public class DateUtilities {
     /**
      * Method sets the format of the DateTimeFormatter property when called and
      * provided and valid dateTimeFormat.
-     *
      * @param dateTimeFormat a <code>DateTimeFormatter</code> object
      * @throws IllegalArgumentException - if format is null
      */
@@ -50,7 +48,6 @@ public class DateUtilities {
     /**
      * Parse the LocalDateTime object to a String and formats the string based
      * upon the format provided.
-     *
      * @param date a <code>LocalDateTime</code> object
      * @param format a <code>DateTimeFormatter</code> object
      * @return LocalDateTime formatted as a string according to the pattern from
@@ -69,7 +66,6 @@ public class DateUtilities {
     /**
      * Converts a string date to a Local Date Time, formatted according to the
      * DateTimeFormatter
-     *
      * @param stringDate a <code>string</code> date value
      * @param format a <code>DateTimeFormatter</code> object
      * @return the string parsed as a LocalDateTime object
@@ -87,7 +83,6 @@ public class DateUtilities {
 
     /**
      * Calculates the time between two days in minutes
-     *
      * @param startDateTime a <code>LocalDateTime</code> object
      * @param endDateTime a <code>LocalDateTime</code> object
      * @return difference in time between two dates in minutes
@@ -106,7 +101,6 @@ public class DateUtilities {
 
     /**
      * Calculates the difference in time between two dates in number hours
-     *
      * @param startDateTime a <code>LocalDateTime</code> object
      * @param endDateTime a <code>LocalDateTime</code> object
      * @return difference in time between two dates in hours
@@ -125,7 +119,6 @@ public class DateUtilities {
 
     /**
      * Calculate the difference between two dates in the number of days
-     *
      * @param startDateTime a <code>LocalDateTime</code> object
      * @param endDateTime a <code>LocalDateTime</code> object
      * @return number of days between two days
@@ -145,7 +138,6 @@ public class DateUtilities {
     /**
      * Gets the future LocalDate from the current date, based upon how many days
      * it should be from the current date
-     *
      * @param numberOfDays - an <code>int</code> value for number of days from the current
      * date
      * @return future date from current date according
@@ -163,7 +155,6 @@ public class DateUtilities {
     /**
      * Calculates the date in the past from the current date according to the
      * specified number of days passed
-     *
      * @param numberOfDays - <code>int</code> value for number of days prior to the current
      * date
      * @return the date prior to the current date
